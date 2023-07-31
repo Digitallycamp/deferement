4c54de04c5415d906acf81f387c508508dd3bb8b
lamodots <lamodots@gmail.com>
2023-07-31 15:26:25 -0700

    Fix error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include "lists.h"
 
-/*
+/**
+* insert_nodeint_at_index - a function that inserts a new
+*node at a given position
 * @head: a pointer to a pointer to a struct
 * @idx: is the index of the list where the new node
 *should be added. Index starts at 0
